d510b32db4fb7d2a9d75e0b7f4449587cad3fa5f
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-12-28 02:24:13 +0700

    PROD SEMINA

--- src/App.js
+++ src/App.js
@@ -1,44 +1,18 @@
-// import { useState } from 'react';
-import { BrowserRouter, Routes, Route, Link, useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-
-function Home(){
-  return <h1>Home</h1>
-};
-
-function Categories(){
-  return <h1>Categories</h1>
-};
-
-function CategoriesDetail(){
-  let params = useParams()
-  return <h1>CategoriesDetail param: {params.id}</h1>
-}
-
-function About(){
-  return <h1>About</h1>
-}
+import { listen } from './redux/listener'
+import { AppRoutes } from './routes';
 
 function App() {
+
+  useEffect(() => {
+    listen()
+  }, []);
+
   return (
     <BrowserRouter>
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/categories'>Categories</Link>
-        </li>
-        <li>
-          <Link to='/about'>About</Link>
-        </li>
-      </ul>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='categories' element={<Categories/>}/>
-        <Route path='categories/:id' element={<CategoriesDetail/>}/>
-        <Route path='about' element={<About/>}/>
-      </Routes>
+      <AppRoutes/>
     </BrowserRouter>
   );
 }
